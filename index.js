e78c80097154f20f12b2391c58502ec60a6b70d6
Souza <souzarodrigo0610@gmail.com>
2025-04-19 16:06:20 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const { AoiClient } = require("aoi.js");
+const fs = require("fs");
+const path = require("path");
+
+// Configurações do bot
+const config = require("./config.js");
+
+// Inicialização do bot
+const client = new AoiClient(config.Bot);
+const canvas = new AoiCanvas(client);
+
+// Função para carregar todos os arquivos JS em subpastas
+function loadFiles(bot, dirPath) {
+	const folders = fs.readdirSync(dirPath);
+
+	for (const folder of folders) {
+		const fullFolderPath = path.join(dirPath, folder);
+		if (fs.statSync(fullFolderPath).isDirectory()) {
+			const files = fs.readdirSync(fullFolderPath).filter(file => file.endsWith('.js'));
+
+			for (const file of files) {
+				const filePath = path.join(fullFolderPath, file);
+				require(filePath)(bot);
+				// console.log(`✅ Carregado: ${filePath}`); 
+			}
+		}
+	}
+}
+
+// Carregar eventos e funções de subpastas
+const typesDir = path.join(__dirname, 'src', 'types');
+loadFiles(client, typesDir);
+
+// Carregar comandos
+client.loadCommands("./src/commands/", true);
